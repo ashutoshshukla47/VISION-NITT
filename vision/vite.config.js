bd5d5ab5b90954930ab7b093b70d46d76b2df990
Ashutosh Shukla <officialashu55@gmail.com>
2026-08-24 19:07:38 +0530

    Update base path in Vite configuration

--- vision/vite.config.js
+++ vision/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/vision-nit/', // <-- match your repo name
+  base: '/VISION-NITT/', // <-- match your repo name
 })
